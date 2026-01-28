90c99d7221531c220e5efd19d1a05741ec65386a
tofikvel <tofig.valiyev1993@gmail.com>
2026-01-28 21:01:54 +0100

    Added new backend features

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,21 +4,21 @@ const users = [
   {
     name: "Admin user",
     email: "admin@email.com",
-    pasword: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
     name: "John Doe",
     email: "john@email.com",
-    pasword: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
   },
   {
     name: "Jane Doe",
     email: "jane@email.com",
-    pasword: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
   },
 ];
 
-export default Users;
+export default users;
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import users from "./data/users";
-import products from "./data/products";
-import User from "./models//userModel";
-import Product from "./models/productModel";
-import Order from "./models/orderModel";
-import connectDB from "./config/db";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -25,6 +25,7 @@ const importData = async () => {
     });
 
     await Product.insertMany(sampleProducts);
+    console.log("Products inserted:", sampleProducts.length);
 
     console.log("Data imported");
     process.exit();
@@ -48,7 +49,7 @@ const destroyData = async () => {
   }
 };
 
-if ((process, argv[2] === "-d")) {
+if (process.argv[2] === "-d") {
   destroyData();
 } else {
   importData();
