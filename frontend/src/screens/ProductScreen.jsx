2ab0077e0f6dace23e74fe0288f79c2c9a17716c
tofikvel <tofig.valiyev1993@gmail.com>
2026-02-02 19:35:48 +0100

    replaced axios with the rtk fetch in product detail page

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,35 +1,23 @@
 import { useParams, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import {
-  Row,
-  Col,
-  Image,
-  ListGroup,
-  Card,
-  Button,
-  ListGroupItem,
-} from "react-bootstrap";
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
-    fetchProduct();
-  }, [productId]);
+  const { data: product, isLoading, error,} = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
-      <Row>
+
+    {isLoading ? (<h2>Loading...</h2>) : error ? (
+      <div>{error?.data?.message || error.error}</div>
+    ) : (
+        <Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -82,6 +70,7 @@ const ProductScreen = () => {
           </Card>
         </Col>
       </Row>
+)}
     </>
   );
 };
